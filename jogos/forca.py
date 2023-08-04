2617678d8a6fdec1c19a03ffc1242b875305ece0
brunofaboci <brunofaboci@gmail.com>
2023-08-04 14:56:06 -0300

    inicio do desenvolvimento do jogo forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,21 @@ def jogar():
     print("Bem vindo ao jogo da Forca!")
     print("***************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Digite uma letra: ").strip().lower()
+        index = 0
+
+        for letra in palavra_secreta:
+            if chute == letra:
+                print("Letra {} encontrada na posição {}".format(letra, index))
+            index += 1
+
+    print("Fim de Jogo")
+
 
 if __name__ == "__main__":
     jogar()
