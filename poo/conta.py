fe66aea0068e9c375d03f27afad4885da506b247
brunofaboci <brunofaboci@gmail.com>
2023-08-16 16:06:45 -0300

    criacao de getters e setters

--- poo/conta.py
+++ poo/conta.py
@@ -6,6 +6,18 @@ class ContaCorrente:
         self.__saldo = saldo
         self.__limite = limite
 
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_limite(self):
+        return self.__limite
+
+    def get_titular(self):
+        return self.__titular
+
+    def set_limite(self, limite):
+        self.__limite = limite
+
     def extrato(self):
         print("Titular: {}".format(self.__titular))
         print("Saldo: {}".format(self.__saldo))
@@ -21,10 +33,13 @@ class ContaCorrente:
         conta_destino.depositar(valor)
 
 
-# conta = ContaCorrente(123, "bruno", 150, 10000)
+conta = ContaCorrente(123, "bruno", 150, 10000)
 # conta2 = ContaCorrente(456, "joao", 200)
 # print(conta.extrato())
 # print(conta.depositar(100))
 # print(conta.extrato())
 # print(conta.sacar(50))
 # print(conta.extrato())
+# print(conta.get_saldo())
+# print(conta.get_limite())
+# print(conta.get_titular())
